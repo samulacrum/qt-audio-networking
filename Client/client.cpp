442aacaf1dcabeccf0a3ca066d18d66bb2f89b10
countsnap <countsnap@gmail.com>
2014-11-02 18:04:31 +0000

    changed client to UDP

--- Client/client.cpp
+++ Client/client.cpp
@@ -2,17 +2,26 @@
 
 Client::Client(QString host, QObject *parent) : QObject(parent)
 {
-    socket = new QTcpSocket(this);
-    socket->connectToHost(host, 8002);
+    socket = new QUdpSocket(this);
+    socket->bind(8002);
+    /*
+    socket->connectToHost(host, 8002);*/
     connect(socket, SIGNAL(readyRead()), this, SLOT(readyRead()));
 }
 
 void Client::readyRead()
 {
+    /*
     QByteArray data;
 
     while (socket->bytesAvailable() > 0)
-        data.append(socket->readAll());
+        data.append(socket->readAll());*/
 
-    output.writeData(data);
+    while (socket->hasPendingDatagrams()) {
+        QByteArray buffer(socket->pendingDatagramSize(), 0);
+        socket->readDatagram(buffer.data(), buffer.size());
+
+        output.writeData(buffer);
+    }
+
 }
--- Client/client.h
+++ Client/client.h
@@ -19,7 +19,7 @@ private slots:
     void readyRead();
 
 private:
-    QTcpSocket *socket;
+    QUdpSocket *socket;
     AudioOutput output;
 };
 
