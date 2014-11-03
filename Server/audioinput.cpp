be579d1ce84d10a84637cdfe248e981d064b55b0
countsnap <countsnap@gmail.com>
2014-11-03 20:25:50 +0000

    re-added the connect()

--- Server/audioinput.cpp
+++ Server/audioinput.cpp
@@ -4,6 +4,7 @@ AudioInput::AudioInput(QAudioDeviceInfo devinfo, QObject *parent)
     : QObject(parent)
     ,   buffer(4096, 0)
 {
+    qDebug("Create Audio");
     format.setChannelCount(1);
     format.setSampleRate(8000);
     format.setSampleSize(16);
@@ -17,9 +18,10 @@ AudioInput::AudioInput(QAudioDeviceInfo devinfo, QObject *parent)
         qWarning() << "Default format not supported - trying to use nearest";
         format = info.nearestFormat(format);
     }
-
+    qDebug("Create Audio2");
     audio = new QAudioInput(devinfo, format, this);
     device = audio->start();
+    connect(device, SIGNAL(readyRead()), this, SLOT(readAudio()));
     /*
 
     QTimer *timer = new QTimer(this);
@@ -34,7 +36,7 @@ void AudioInput::readAudio()
 
     //Check the number of samples in input buffer
     qint64 len = audio->bytesReady();
-
+    qDebug() << len;
     if (len > 4096) {
         len = 4096;
     }
