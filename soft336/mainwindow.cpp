1245cb45203f66a658bb7641d0e5ebaacfd896c9
countsnap <countsnap@gmail.com>
2014-11-04 01:27:25 +0000

    finished merge

--- soft336/mainwindow.cpp
+++ soft336/mainwindow.cpp
@@ -6,9 +6,37 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    setFixedSize(size()); //prevent window resizing
+    getDeviceInfo(); //read available input devices
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_listenButton_clicked()
+{
+    client = new Client(this);
+    ui->listenButton->setEnabled(false);
+}
+
+void MainWindow::getDeviceInfo()
+{
+    QList<QAudioDeviceInfo> devices = QAudioDeviceInfo::availableDevices(QAudio::AudioInput);
+    if (devices.isEmpty()) QMessageBox::warning(this, "Error", "No input device found!");
+    for(int i = 0; i < devices.size(); ++i)
+        ui->deviceComboBox->addItem(devices.at(i).deviceName(), qVariantFromValue(devices.at(i)));
+}
+
+void MainWindow::on_broadcastButton_clicked()
+{
+    QAudioDeviceInfo devinfo = ui->deviceComboBox->itemData(ui->deviceComboBox->currentIndex()).value<QAudioDeviceInfo>();
+    input = new AudioInput(devinfo, this);
+    server = new Server(this);
+    connect(input, SIGNAL(dataReady(QByteArray)), server, SLOT(writeData(QByteArray)));
+
+    ui->deviceComboBox->setEnabled(false);
+    ui->broadcastButton->setEnabled(false);
 }
--- soft336/mainwindow.h
+++ soft336/mainwindow.h
@@ -2,6 +2,12 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QtGui>
+#include <QtWidgets>
+
+#include "client.h"
+#include "server.h"
+#include "audioinput.h"
 
 namespace Ui {
 class MainWindow;
@@ -15,8 +21,16 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+    void getDeviceInfo();
+    void on_listenButton_clicked();
+    void on_broadcastButton_clicked();
+
 private:
     Ui::MainWindow *ui;
+    Client  *client;
+    Server *server;
+    AudioInput *input;
 };
 
 #endif // MAINWINDOW_H
