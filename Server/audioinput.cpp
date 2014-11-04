cd248317157bd4afa44ccbe90fda58fd59b84104
countsnap <countsnap@gmail.com>
2014-11-04 00:44:54 +0000

    optimised audio format

--- Server/audioinput.cpp
+++ Server/audioinput.cpp
@@ -4,14 +4,14 @@ AudioInput::AudioInput(QAudioDeviceInfo devinfo, QObject *parent) : QObject(pare
 {
     QAudioFormat format;
     format.setChannelCount(1);
-    format.setSampleRate(8000);
-    format.setSampleSize(8);
+    format.setSampleRate(44100);
+    format.setSampleSize(16);
     format.setCodec("audio/pcm");
     format.setByteOrder(QAudioFormat::LittleEndian);
     format.setSampleType(QAudioFormat::SignedInt);
 
     audio = new QAudioInput(devinfo, format, this);
-    audio->setBufferSize(320);
+    audio->setBufferSize(1024);
     device = audio->start();
 
     connect(device, SIGNAL(readyRead()), this, SLOT(readyRead()));
@@ -25,8 +25,8 @@ void AudioInput::readyRead()
     qint64 len = audio->bytesReady();
     qint64 l;
 
-    if(len > 320)
-        len = 320;
+    if(len > 1024)
+        len = 1024;
 
     //Read sound samples from input device to buffer
     if (len > 0) {
